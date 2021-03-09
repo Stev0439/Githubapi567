046ac85213806d7d8c943bf2f3674833c1a186f5
Stev0439 <62709228+Stev0439@users.noreply.github.com>
2021-03-09 18:22:08 -0500

    Update Gitrepos.py

--- Gitrepos.py
+++ Gitrepos.py
@@ -12,9 +12,9 @@ def gitrepo(userID):
         request = requests.get("https://api.github.com/users/{}/repos".format(userID))
         repos = request.json()
         for repo in repos:
-            repoRequest = requests.get("https://api.github.com/repos/{}/{}/commits".format(userID, repo["name"]))
+            repoRequest = requests.get("https://api.github.com/repos/{}/{}/commits".format(userID, repo['name']))
             commits = repoRequest.json()
-            repocommits.append("Repo: {} Number of commits: {}".format(repo["name"]), len(commits))
+            repocommits.append("Repo: {} Number of commits: {}".format(repo['name']), len(commits))
         return repocommits
 if __name__ == '__main__':
     userID = input("Enter Github user ID:")
