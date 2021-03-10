6645b801879e0e53f11576f633f57c5d30c19b73
Stev0439 <62709228+Stev0439@users.noreply.github.com>
2021-03-09 20:51:22 -0500

    Update test_gitrepo.py

--- test_gitrepo.py
+++ test_gitrepo.py
@@ -10,8 +10,12 @@ from Gitrepos import gitrepo
 class Test(unittest.TestCase):
     
     def test_richkempinski(self):
-        richrepos = []
         self.assertEqual(len(gitrepo("richkempinski")),8,'there are 8 repos')
+    def test_Stev0439(self):
+        self.assertEqual(gitrepo("Stev0439")["567Trangle"],9, "there are 9 commits")
+        self.assertEqual(gitrepo("Stev0439")["helloworld"],3, "there are 3 commits")
+        self.assertEqual(gitrepo("Stev0439")["Homework-3"],3, "there are 3 commits")
+        self.assertEqual(gitrepo("Stev0439")["Triangle"],3, "there are 3 commits")
 
 
 if __name__ == "__main__":
