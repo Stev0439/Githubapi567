e73a8004aa91a4aa5bd24469715b6ed9d94d058e
Stev0439 <62709228+Stev0439@users.noreply.github.com>
2021-03-16 19:01:17 -0400

    Update test_gitrepo.py

--- test_gitrepo.py
+++ test_gitrepo.py
@@ -9,7 +9,7 @@ from Gitrepos import gitrepo
 
 class Test(unittest.TestCase):
     @mock.patch('Gitrepos.gitrepo', return_value= [1,2,3,4,5,6,7,8,9])
-    def test_richkempinski(self):
+    def test_richkempinski(self, mock_output):
         self.assertEqual(len(gitrepo("richkempinski")),9,'there are 9 repos')
     def test_Stev0439(self):
         self.assertEqual(gitrepo("Stev0439")["567Trangle"],9, "there are 9 commits")
