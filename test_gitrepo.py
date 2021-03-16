6040c33ed7b40d1c1eb4b4d82a2e5c161633c5be
Stev0439 <62709228+Stev0439@users.noreply.github.com>
2021-03-16 19:06:08 -0400

    Update test_gitrepo.py

--- test_gitrepo.py
+++ test_gitrepo.py
@@ -11,8 +11,9 @@ class Test(unittest.TestCase):
     @mock.patch('Gitrepos.gitrepo', return_value= [1,2,3,4,5,6,7,8,9])
     def test_richkempinski(self, mock_output):
         self.assertEqual(len(gitrepo("richkempinski")),9,'there are 9 repos')
+    @mock.patch('Gitrepos.gitrepo', return_value= {'567Trangle': 11,'helloworld': 3,'Homework-3': 3,'Triangle': 3,} )
     def test_Stev0439(self):
-        self.assertEqual(gitrepo("Stev0439")["567Trangle"],9, "there are 9 commits")
+        self.assertEqual(gitrepo("Stev0439")["567Trangle"],11, "there are 11 commits")
         self.assertEqual(gitrepo("Stev0439")["helloworld"],3, "there are 3 commits")
         self.assertEqual(gitrepo("Stev0439")["Homework-3"],3, "there are 3 commits")
         self.assertEqual(gitrepo("Stev0439")["Triangle"],3, "there are 3 commits")
